60838c176a4c29b9ffaad7c053e18b5436692e17
chauffemain <46550908+chauffemain@users.noreply.github.com>
2019-01-27 18:27:44 -0600

    hiding question ter

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Joke = props => (
   <div>
-    <h3>Question: {props.control.question}</h3>
+    <h3 style={{ display: props.control.question ? "block" : "none" }}>
+      Question: {props.control.question}
+    </h3>
     <h3>Answer: {props.control.answer}</h3>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-
+//import styles
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
